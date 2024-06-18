8dfc654eaf7137e276c93b7ece2d63c7cb29a120
kryptoNkn <myersalp@gmail.com>
2024-06-18 23:44:26 +0300

    Added sound settings (music)

---
+++ SurvivalGame/Assets/Scripts/MainMenu/Settings/VolumeSettings.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class VolumeSettings : MonoBehaviour
+{
+   private AudioSource audioSrc;
+   private float musicVolume = 1f;
+
+   private void Start()
+   {
+      audioSrc = GetComponent<AudioSource>();
+   }
+
+   private void Update()
+   {
+      audioSrc.volume = musicVolume;
+   }
+
+   public void SetVolume(float vol)
+   {
+      musicVolume = vol;
+   }
+}
